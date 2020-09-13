dfeca7fa03596f847f74f8f8cb84fb0789eee7f2
Luiz Fernando Vicentin Pavanello <luizpavanello@gmail>
2020-09-13 11:11:14 -0300

    Update

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
+function formatMoney(value) {
+    value = Math.ceil(value * 100) / 100
+    value = value.toFixed(2)
+    return '$' + value
+}
+
+function formatSplit(value) {
+    if (value === '1' ) {
+        return value + ' ' + 'person' 
+    } return value + ' ' + 'people' 
+}
+
 function update() {
-    let bill = document.getElementById('yourBill').value
+    let bill = Number(document.getElementById('yourBill').value)
     let tipPercent = document.getElementById('tipInput').value
     let split = document.getElementById('splitInput').value
-    console.log({bill, tipPercent, split})
+    
+    let tipValue = bill * (tipPercent / 100)
+    let tipEach = tipValue / split
+    let newBillEach = (bill + tipValue) / split
+
+    document.getElementById('tipPercent').innerHTML = tipPercent + '%'
+    document.getElementById('tipValue').innerHTML = formatMoney(tipValue)
+    document.getElementById('totalWithTip').innerHTML = formatMoney(bill + tipValue)
+    document.getElementById('splitValue').innerHTML = formatSplit(split)
+    document.getElementById('billEach').innerHTML = formatMoney(newBillEach)
+    document.getElementById('tipEach').innerHTML = formatMoney(tipEach)
+
+
 }
 
 let container = document.getElementById('container')
